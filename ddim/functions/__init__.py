bd2920bf672875d0bae327e05970eed777e06190
dzy-cxy <77495162+dzy-cxy@users.noreply.github.com>
2024-01-18 11:11:52 -0500

    Add files via upload

---
+++ ddim/functions/__init__.py
@@ -0,0 +1,28 @@
+import torch.optim as optim
+
+
+def get_optimizer(config, parameters):
+    if config.optim.optimizer == 'Adam':
+        return optim.Adam(filter(lambda p:p.requires_grad, parameters), lr=config.optim.lr, weight_decay=config.optim.weight_decay,
+                          betas=(config.optim.beta1, 0.999), amsgrad=config.optim.amsgrad,
+                          eps=config.optim.eps)
+    elif config.optim.optimizer == 'RMSProp':
+        return optim.RMSprop(filter(lambda p:p.requires_grad, parameters), lr=config.optim.lr, weight_decay=config.optim.weight_decay)
+    elif config.optim.optimizer == 'SGD':
+        return optim.SGD(filter(lambda p:p.requires_grad, parameters), lr=config.optim.lr, momentum=0.9)
+    else:
+        raise NotImplementedError(
+            'Optimizer {} not understood.'.format(config.optim.optimizer))
+    
+# def get_optimizer(config, parameters):
+#     if config.optim.optimizer == 'Adam':
+#         return optim.Adam(parameters, lr=config.optim.lr, weight_decay=config.optim.weight_decay,
+#                           betas=(config.optim.beta1, 0.999), amsgrad=config.optim.amsgrad,
+#                           eps=config.optim.eps)
+#     elif config.optim.optimizer == 'RMSProp':
+#         return optim.RMSprop(parameters, lr=config.optim.lr, weight_decay=config.optim.weight_decay)
+#     elif config.optim.optimizer == 'SGD':
+#         return optim.SGD(parameters, lr=config.optim.lr, momentum=0.9)
+#     else:
+#         raise NotImplementedError(
+#             'Optimizer {} not understood.'.format(config.optim.optimizer))
